04233ab352f6a1c4cc3c164d6752cebeca1595ab
Pierce Josse Maximilliano Guntoro <piercejosse@gmail.com>
2025-10-18 16:54:14 +0700

    18 10 rev 2
    wokring on app js indexjs and style.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import "./styles.css";
-
-export default function ListingAd({ pic, title, address, description }) {
+export default function ListingAd({icon, pic, title, address, description ,availabilities_label, subprice_label}) {
   return (
     <div className="App">
-      <img className="mainPic" width="300" height="500" src={pic} />
+      <img className="mainPic" width="544" height="272" src={pic} />
       <div className="mainContent">
-        <h1>{title}</h1>
-        <p className="address">{address}</p>
-        <button>See description</button>
-        <p className="description">{description}</p>
+
+        <div className="topContent">
+            <div className="leftContent">
+              <div className="icon" width="40" height="40" src={icon}></div>
+              <div className="title">
+                <h1>{title}</h1>
+              <div/>
+                <p className="address">{address}</p>
+                <p className="label">{availabilities_label}</p>
+              </div>
+            </div>
+            <div className="rightContent">
+              <div className="price">
+                <h1>$2,609 - $3,043 psf</h1>
+              </div>
+              <p className="priceFrom">{subprice_label}</p>
+            </div>
+        </div>
+
+        <div className="bottomContent">
+          <button>See description</button>
+          <p className="description">{description}</p>
+        </div>
+
       </div>
     </div>
   );
